e6045c1dd1528bfeaa2f413fb1e3d0d590d36575
Gwiny7 <guischwetz@gmail.com>
2024-08-26 22:17:19 -0300

    SetupInicialDeScripts

---
+++ Bullet_Holes/Assets/Scripts/InputReader.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace BulletHoles
+{
+    [RequireComponent(typeof(PlayerInput))]
+    public class InputReader : MonoBehaviour
+    {
+       PlayerInput playerInput;
+       InputAction moveAction;
+
+       public Vector2 Move => moveAction.ReadValue<Vector2>();
+
+       void Start() {
+            playerInput = GetComponent<PlayerInput>();
+            moveAction = playerInput.actions["Move"];
+       }
+    }
+}
---
+++ Bullet_Holes/Assets/Scripts/ParallaxController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace BulletHoles
+{
+    public class ParallaxController : MonoBehaviour
+    {
+        
+    }
+}
