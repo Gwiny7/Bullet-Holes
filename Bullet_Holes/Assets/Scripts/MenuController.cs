7bca5cd1ce4dc6e2d4d8bc480ed4ee1fda5a7fcc
IanBoniatti <boniatti2002@gmail.com>
2024-08-28 16:34:25 -0300

    MenuPrefab
    Na pasta Prefab botei o menu pré-pronto.

---
+++ Bullet_Holes/Assets/Scripts/MenuController.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class MenuController : MonoBehaviour
+{
+
+    public GameObject mainMenuPanel;
+    public GameObject optionsPanel;
+    public GameObject creditsPanel;
+    public Slider volumeSlider;
+    public Dropdown resolutionDropdown;
+    private List<Resolution> resolutions = new List<Resolution>();
+
+    private void Start()
+    {
+        volumeSlider.onValueChanged.AddListener(SetVolume);
+
+        PopulateResolutionDropdown();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("NomeDaSuaCenaDeJogo");
+    }
+
+    public void OpenOptions()
+    {
+        mainMenuPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void SetVolume(float volume) //Volume
+    {
+        AudioListener.volume = volume;
+    }
+
+    void PopulateResolutionDropdown()   //Resolução da tela
+    {
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+
+        foreach (Resolution resolution in Screen.resolutions)
+        {
+            resolutions.Add(resolution);
+            string option = resolution.width + " x " + resolution.height;
+            options.Add(option);
+
+            if (resolution.width == Screen.currentResolution.width && resolution.height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = resolutions.Count - 1;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        resolutionDropdown.onValueChanged.AddListener(delegate { SetResolution(resolutionDropdown.value); });
+    }
+
+    public void SetResolution(int resolutionIndex)  //Resolução da tela
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void CloseOptions()
+    {
+        optionsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void OpenCredits()
+    {
+        mainMenuPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        creditsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+}
